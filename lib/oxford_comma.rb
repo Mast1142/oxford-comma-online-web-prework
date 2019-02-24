4d3d16e10a06d82168830ffdb9b87851ccd27da4
Samuel W Tong <ssamt1142@gmail.com>
2019-02-24 23:43:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,13 +4,10 @@ def oxford_comma(array)
   elsif array.count == 2
     array*" and "
   else
-    
-end
-
-def oxford_comma(array)
-  array*" and "
- last = array.pop
+    last = array.pop
  last_fruit = ", and " << last
  front_fruits = array.join(", ")
  front_fruits << last_fruit
  end
+ end
+end
