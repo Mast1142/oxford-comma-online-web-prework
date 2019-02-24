f84ba405c517b6c6503076ef9ed48a9acdd7c056
Samuel W Tong <ssamt1142@gmail.com>
2019-02-24 22:54:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+  array*","
+end
 
+def oxford_comma(array)
+  array*" and "
+end
+
+def oxford_comma(array)
+ last = array.pop
+ last.join(", and")
+ right = array.join(",") 
+ together = right << last
+ put together
 end
